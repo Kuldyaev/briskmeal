807c8a276e36a0f5ffde38a97aef93c6900f4bd7
Kuldyaev Viacheslav <slavalion1@gmail.com>
2022-12-17 18:29:11 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/catalog', function () {
+    return view('catalog');
+});
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
