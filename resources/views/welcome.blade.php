c8274850154c794e2dcfe623af38a697bbd42d5d
Kuldyaev Viacheslav <slavalion1@gmail.com>
2022-12-18 00:32:08 +0300

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,9 +22,10 @@
     </head>
     <body class="antialiased">
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
-            <a href="{{ url('/catalog') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Catalog</a>
+            
             @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+                        <a href="{{ url('/catalog') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Catalog</a>
                     @auth
                         <a href="{{ url('/home') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Home</a>
                     @else
