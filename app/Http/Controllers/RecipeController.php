561c3d929841340996c3afe8b40d0d6407925e70
Kuldyaev Viacheslav <slavalion1@gmail.com>
2023-01-08 20:07:00 +0300

    Create RecipeController.php

---
+++ app/Http/Controllers/RecipeController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+
+use App\Queries\RecipesQueryBuilder;
+use App\Queries\WeekQueryBuilder;
+
+class RecipeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(WeekQueryBuilder $builder, RecipesQueryBuilder $builder_recipes)
+    {
+        try {
+            $active_weeks = $builder->getActiveWeeks();
+            $first_active_week = $active_weeks->value('week_name');
+            $recipes_id = $builder->getRecipesByWeek($first_active_week);
+
+            return view('catalog')->with('recipes', $builder_recipes->getRecipesById($recipes_id))
+                ->with('activeWeeks', $active_weeks->toJson());
+        } catch (ModelNotFoundException $e) {
+            return back()->withError('error', $e->getMessage());
+        }
+    }
+
+    public function show(RecipesQueryBuilder $builder, $id)
+    {
+        return view('recipe')->with('recipe', $builder->getOneRecipe($id));
+    }
+}
