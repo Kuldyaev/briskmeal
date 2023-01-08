defb3b1fface1bd0487792d140a6010319e4bd37
Kuldyaev Viacheslav <slavalion1@gmail.com>
2023-01-08 20:04:46 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,20 +2,19 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\RecipeController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
 */
 
 Route::get('/', function () {return view('welcome');})->name('home');
 
-Route::get('/catalog', function () {return view('catalog');})->name('catalog');
+Route::get('/catalog', [RecipeController::class, 'index'])
+    ->name('catalog');
 
 Route::get('/meal/{meal}', function () {return view('oneMeal');})->name('oneMeal');
 
