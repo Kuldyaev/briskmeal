b077a6cb3d6808115dbf380081c8c12fa596488f
Kuldyaev Viacheslav <slavalion1@gmail.com>
2023-01-11 00:32:03 +0300

    Update RecipeController.php

--- app/Http/Controllers/RecipeController.php
+++ app/Http/Controllers/RecipeController.php
@@ -23,7 +23,7 @@ class RecipeController extends Controller
             $recipes_id = $builder->getRecipesByWeek($first_active_week);
               
             return view('catalog')->with('recipes', $builder_recipes->getRecipesById($recipes_id))
-                ->with('activeWeeks', $builder->getActiveWeeks()->toJson());
+                ->with('activeWeeks','xxxx');
         } catch (ModelNotFoundException $e) {
             return back()->withError('error', $e->getMessage());
         }
